4104bd065d5bdf3d16df9d45d7d6d869a30e00ad
EwanSC <ewan.coopey@hdr.mq.edu.au>
2021-12-17 18:42:02 +1100

    man this is hard

---
+++ scripts/cleaning_epig_scrape.R
@@ -0,0 +1,61 @@
+# Cleaning Epig Scrape Data
+# ESC 17/12/2021 
+# Aim is to clean the most recent data from 
+
+library(tidyverse)
+
+# get the data using premade function
+
+RawEpigData <- load_epig_data("data/2021-11-16-EDCS_via_Lat_Epig-prov_Dalmatia-10140.json")
+
+str(RawEpigData)
+
+head(RawEpigData)
+
+# lets remove unnecessary columns
+
+SelectEpigData <- RawEpigData %>%
+  select(`EDCS-ID`,publication,province,place,`dating from`,`dating to`,status,inscription,`inscription interpretive cleaning`,Latitude,Longitude)
+
+# lets clean 'place'
+# first count it
+
+SumEDPlace <-  SelectEpigData %>% 
+  group_by(place) %>% 
+  count(place)
+
+#lets practice making all major sites one location with ifelse
+
+CleanerEDPlace <- SelectEpigData %>%
+  mutate(`ancient place`=ifelse(place %in% c('Burnum',
+                                             'Ivosevci / Burnum',
+                                             'Karlovac / Karlstadt / Burnum',
+                                             'Kistanje / Burnum','Knin / Burnum',
+                                             'Mokro Polje / Burnum',
+                                             'Puljane / Pugliane / Burnum',
+                                             'Strmca / Stermizza / Burnum'), 
+                                'Burnum', place)) %>%
+  mutate(`ancient place`=ifelse(place %in% c('Gardun / Tilurium',
+                                             'Trilj / Tilurium',
+                                             'Vojnic Sinjski / Tilurium'), 
+                                'Tilurium', `ancient place`)) %>%
+  mutate(`ancient place`=ifelse(place %in% c('Kastel Sucurac / Salona',
+                                             'Manastirine / Salona',
+                                             'Pazdigrad / Pazdigrada / Salona',
+                                             'Solin / Salona',
+                                             'Vrlika / Vrlica / Salona'), 
+                                'Salona', `ancient place`))
+
+SumCleanerEDPlace <-  CleanerEDPlace %>% 
+  group_by(`ancient place`) %>% 
+  count(`ancient place`)
+
+CleanEDPlace <- CleanerEDPlace %>%
+  select(`EDCS-ID`,publication,province,`ancient place`,`dating from`,`dating to`,status,inscription,`inscription interpretive cleaning`,Latitude,Longitude)
+
+##works, but there must be a quicker way...
+
+## lets have a look at status
+SumEDStatus <-  SelectEpigData %>% 
+  group_by(status) %>% 
+  count(status)
--- scripts/primary_epigraphic_cor(e)pus.R
+++ scripts/primary_epigraphic_cor(e)pus.R
@@ -1,23 +1,11 @@
-library(rjson) #https://www.datacamp.com/community/tutorials/r-data-import-tutorial#javascript
-library(tibble)
-library(data.table)
-library(dplyr)
-
-json_filename <- "data/2021-11-16-EDCS_via_Lat_Epig-prov_Dalmatia-10140.json"
-
-json_data <- fromJSON(file= json_filename )
-
-json_epig_data <- json_data$data
-
-epig_data <- data.table::rbindlist(json_epig_data, fill = TRUE) #https://stackoverflow.com/questions/53037844/multiple-list-json-to-data-frame-in-r
-
-firstcent_epig_data <- epig_data %>%
-  filter(`dating from` %in% (-30:100), `dating to` %in% (9:150)) %>%
-  arrange("dating to", "dating from")
-
-# make into a function
-# function that imports, cleans and creates df for 1st century epig in Dalmatia
-# following https://swcarpentry.github.io/r-novice-inflammation/02-func-R/index.html
+# Primary epigraphic Cor(e)pus
+# ESC 17/12/2021
+# Aim is to turn the LatEpig JSON files into a DF and then into a 1st century CE df
+# as a function function which does just this
+# Information from:
+# https://www.datacamp.com/community/tutorials/r-data-import-tutorial#javascript
+# https://stackoverflow.com/questions/53037844/multiple-list-json-to-data-frame-in-r
+# https://swcarpentry.github.io/r-novice-inflammation/02-func-R/index.html
 
 load_epig_data <- function(datascrape) {
   library(rjson)
