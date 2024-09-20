9116465ec3e0ea6d1690147a2c7078a938fbde0f
EwanSC <ewansc4@gmail.com>
2024-09-20 13:50:51 +1000

    finished making functions for scripts, and creating new plots / data / analysis for 'military' not filtered by place

--- scripts/4.plotting-dates-LIRE.R
+++ scripts/4.plotting-dates-LIRE.R
@@ -1,9 +1,10 @@
 # Plotting dates, LIRE
 # Made 16/9/24
 # Edited: 16/9/24
-### FIRST RUN /scripts/4.primary_epigraphic_cor(e)pus-LIRE.R
+### FIRST RUN /scripts/2.corpuss-LIRE.R
 ## using https://cran.r-project.org/web/packages/datplot/datplot.pdf
 ## and https://cran.r-project.org/web/packages/datplot/vignettes/how-to.html 
+library(dplyr)
 library(datplot)
 library(ggplot2)
 library(sqldf)
@@ -16,67 +17,46 @@ count(LIRE_Dal_corpus_no_place_filtering_clean)
 count(LIRE_Dal)
 count(LIRE_Dal_clean)
 
+# distinguish between the two types by adding variable column
+
 LIRE_Dal_corpus$variable <- "Military"
-LIRE_Dal_corpus_no_place_filtering <- "Military"
+LIRE_Dal_corpus_no_place_filtering$variable <- "Military"
 LIRE_Dal_corpus_clean$variable <- "Military"
-LIRE_Dal_corpus_no_place_filtering_clean <- "Military"
+LIRE_Dal_corpus_no_place_filtering_clean$variable <- "Military"
 LIRE_Dal$variable <- "All"
 LIRE_Dal_clean$variable <- "All"
 
-LIRE_Dal_corpus_dates <- na.omit(LIRE_Dal_corpus %>%
-                                   select(`LIST-ID`,variable,not_before,not_after))
+# make function to clean to only required data for aorist analysis
+# this includes modifying column classes and removing nulls 
 
-LIRE_Dal_corpus_clean_dates <- na.omit(LIRE_Dal_corpus_clean %>%
-                                         select(`LIST-ID`,variable,not_before,not_after))
+prepare_for_density <- function(dataframe) {
+  library(dplyr)
+  library(tidyverse)
+  prepare_for_density_dates <- na.omit(dataframe %>%
+               select(`LIST.ID`,variable,not_before,not_after))
+  prepare_for_density_altered <- type.convert(prepare_for_density_dates, as.is = TRUE)    
+  prepare_for_density <- na.omit(prepare_for_density_altered)
+  return(prepare_for_density)
+}
 
-LIRE_Dal_corpus_npf_dates <- na.omit(LIRE_Dal_corpus %>%
-                                   select(`LIST-ID`,variable,not_before,not_after))
-
-LIRE_Dal_corpus_npf_clean_dates <- na.omit(LIRE_Dal_corpus_clean %>%
-                                         select(`LIST-ID`,variable,not_before,not_after))
-
-LIRE_Dal_dates <- na.omit(LIRE_Dal %>%
-                                   select(`LIST-ID`,variable,not_before,not_after))
-
-LIRE_Dal_clean_dates <- na.omit(LIRE_Dal_clean %>%
-                                   select(`LIST-ID`,variable,not_before,not_after))
-
-
-LIRE_Dal_corpus_altered <- type.convert(LIRE_Dal_corpus_dates, as.is = TRUE)    # Modify column classes
-LIRE_Dal_corpus_na <- na.omit(LIRE_Dal_corpus_altered)  #remove nulls
-LIRE_Dal_corpus_na                                      # Print updated data frame
-
-LIRE_Dal_corpus_clean_altered <- type.convert(LIRE_Dal_corpus_clean_dates, as.is = TRUE)    
-LIRE_Dal_corpus_clean_na <- na.omit(LIRE_Dal_corpus_clean_altered) 
-LIRE_Dal_corpus_clean_na
-
-LIRE_Dal_corpus_npf_altered <- type.convert(LIRE_Dal_corpus_npf_dates, as.is = TRUE) 
-LIRE_Dal_corpus_npf_na <- na.omit(LIRE_Dal_corpus_npf_altered)
-LIRE_Dal_corpus_npf_na
-
-LIRE_Dal_corpus_npf_clean_altered <- type.convert(LIRE_Dal_corpus_npf_clean_dates, as.is = TRUE)
-LIRE_Dal_corpus_npf_clean_na <- na.omit(LIRE_Dal_corpus_npf_clean_altered)
-LIRE_Dal_corpus_npf_clean_na
-
-LIRE_all_Dal_dates_altered <- type.convert(LIRE_all_Dal_dates, as.is = TRUE) 
-LIRE_all_Dal_dates_na <- na.omit(LIRE_all_Dal_dates_altered)
-LIRE_all_Dal_dates_na
-
-LIRE_all_Dal_clean_dates_altered <- type.convert(LIRE_all_Dal_clean_dates, as.is = TRUE)
-LIRE_all_Dal_clean_dates_na <- na.omit(LIRE_all_Dal_clean_dates_altered) 
-LIRE_all_Dal_clean_dates_na
+# use function
+LIRE_Dal_corpus_na <- prepare_for_density(LIRE_Dal_corpus)
+LIRE_Dal_corpus_npf_na <- prepare_for_density(LIRE_Dal_corpus_no_place_filtering)
+LIRE_Dal_corpus_clean_na <- prepare_for_density(LIRE_Dal_corpus_clean)
+LIRE_Dal_corpus_npf_clean_na <- prepare_for_density(LIRE_Dal_corpus_no_place_filtering_clean)
+LIRE_Dal_na <- prepare_for_density(LIRE_Dal)
+LIRE_Dal_clean_na <- prepare_for_density(LIRE_Dal_clean)
 
 LIRE_Dal_corpus_count <- count(LIRE_Dal_corpus_na)
 LIRE_Dal_corpus_npf_count <- count(LIRE_Dal_corpus_npf_na)
 LIRE_Dal_corpus_clean_count <- count(LIRE_Dal_corpus_clean_na)
 LIRE_Dal_corpus_npf_clean_count <- count(LIRE_Dal_corpus_npf_clean_na)
-LIRE_all_Dal_count <- count(LIRE_all_Dal_dates_na)
-LIRE_all_Dal_clean_count <- count(LIRE_all_Dal_clean_dates_na)
+LIRE_Dal_count <- count(LIRE_Dal_na)
+LIRE_Dal_clean_count <- count(LIRE_Dal_clean_na)
 
 # check distribution of monuments using clean
 ## Following https://static.cambridge.org/content/id/urn:cambridge.org:id:article:S2326376821000085/resource/name/S2326376821000085sup001.pdf
 LIRE_Dal_corpus_clean %>%
-  select(`LIST-ID`,province_label_clean,not_before,not_after) %>%
   mutate(timespan = not_after-not_before) %>%
   ggplot(aes(x = timespan)) +
   geom_histogram(binwidth = 10, fill = "#FF8247") +
@@ -87,13 +67,30 @@ LIRE_Dal_corpus_clean %>%
                        LIRE_Dal_corpus_clean_count$n,
                        sep = "",
                        ". Data from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
+                       "Filtered by key words, tags, and places.\n",
                        "Method = Steinmann & Weissova 2021."))
 
 ggsave("output_images/chronological_distribution/LIRE_inscription_ranges_corpus.jpeg",
        dpi = 600)
 
-LIRE_all_Dal_clean %>%
-  select(`LIST-ID`,province_label_clean,not_before,not_after) %>%
+LIRE_Dal_corpus_no_place_filtering_clean %>%
+  mutate(timespan = not_after-not_before) %>%
+  ggplot(aes(x = timespan)) +
+  geom_histogram(binwidth = 10, fill = "#FF8247") +
+  labs(title = "Inscription date range",
+       subtitle = "Dalmatia: Military",
+       x = "Length of timespan (10 year bins)", y = "Number of inscriptions",
+       caption = paste("n = ",
+                       LIRE_Dal_corpus_npf_clean_count$n,
+                       sep = "",
+                       ". Data from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
+                       "Filtered by key words and tags.\n",
+                       "Method = Steinmann & Weissova 2021."))
+
+ggsave("output_images/chronological_distribution/LIRE_inscription_ranges_corpus_no_place_filter.jpeg",
+       dpi = 600)
+
+LIRE_Dal_clean %>%
   mutate(timespan = not_after-not_before) %>%
   ggplot(aes(x = timespan)) +
   geom_histogram(binwidth = 10, fill = "#009E73") +
@@ -101,7 +98,7 @@ LIRE_all_Dal_clean %>%
        subtitle = "Dalmatia: All categories",
        x = "Length of timespan (10 year bins)", y = "Number of inscriptions",
        caption = paste("n = ",
-                       LIRE_all_Dal_clean_count$n,
+                       LIRE_Dal_clean_count$n,
                        sep = "",
                        ". Data from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
                        "Method = Steinmann & Weissova 2021."))
@@ -111,11 +108,9 @@ ggsave("output_images/chronological_distribution/LIRE_inscription_ranges_Dalmati
 
 # now for weitghed density using datplot and 'clean' dfs
 ## Work with https://cran.r-project.org/web/packages/datplot/vignettes/how-to.html
-LIRE_Dal_corpus_clean_scaled <- scaleweight(datsteps(LIRE_Dal_corpus_clean_na, stepsize = 25),
+LIRE_Dal_corpus_clean_scaled <- scaleweight(datsteps(LIRE_Dal_corpus_clean_na,
+                                                     stepsize = 25),
                                             var = "all")
-
-LIRE_all_Dal_clean_scaled <- scaleweight(datsteps(LIRE_all_Dal_clean_dates_na, stepsize = 25),
-                                         var = "all")
 
 ggplot(data = LIRE_Dal_corpus_clean_scaled, aes(x = DAT_step, weight = weight)) +
   geom_density(alpha = 0.8, fill = "#FF8247") +
@@ -123,7 +118,8 @@ ggplot(data = LIRE_Dal_corpus_clean_scaled, aes(x = DAT_step, weight = weight)) 
        caption = paste("n = ",
                        LIRE_Dal_corpus_clean_count$n,
                        sep = "",
-                       "Data from LIRE v.3.0 (Cleaned province = Dalmatia)."),
+                       ". Data from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
+                       "Filtered by key words, tags, and places."),
        title = "Chronological distribution of military monuments in Dalmatia",
        subtitle = paste("Using the weighted output of datsteps() ",
                            "with stepsize of ",
@@ -133,36 +129,66 @@ ggplot(data = LIRE_Dal_corpus_clean_scaled, aes(x = DAT_step, weight = weight)) 
 ggsave("output_images/chronological_distribution/LIRE_clean_corpus_plot.jpeg",
        width = 180, height = 100, unit = "mm", dpi = 600)
 
-ggplot(data = LIRE_all_Dal_clean_scaled, aes(x = DAT_step, weight = weight)) +
+LIRE_Dal_corpus_npf_clean_scaled <- scaleweight(datsteps(LIRE_Dal_corpus_npf_clean_na,
+                                                         stepsize = 25),
+                                                var = "all")
+
+ggplot(data = LIRE_Dal_corpus_npf_clean_scaled, aes(x = DAT_step, weight = weight)) +
+  geom_density(alpha = 0.8, fill = "#FF8247") +
+  labs(x = "Date (BCE/CE)", y = "Density",
+       caption = paste("n = ",
+                       LIRE_Dal_corpus_npf_clean_count$n,
+                       sep = "",
+                       ". Data from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
+                       "Filtered by key words and tags."),
+       title = "Chronological distribution of military monuments in Dalmatia",
+       subtitle = paste("Using the weighted output of datsteps() ",
+                        "with stepsize of ",
+                        attributes(LIRE_Dal_corpus_npf_clean_scaled)$stepsize,
+                        sep = ""))
+
+ggsave("output_images/chronological_distribution/LIRE_clean_corpus_plot_no_place_filter.jpeg",
+       width = 180, height = 100, unit = "mm", dpi = 600)
+
+LIRE_Dal_clean_scaled <- scaleweight(datsteps(LIRE_Dal_clean_na,
+                                              stepsize = 25),
+                                     var = "all")
+
+ggplot(data = LIRE_Dal_clean_scaled, aes(x = DAT_step, weight = weight)) +
   geom_density(alpha = 0.8, fill = "#009E73") +
   labs(x = "Date (BCE/CE)", y = "Density",
        caption = paste("n = ",
-                       LIRE_all_Dal_clean_count$n,
+                       LIRE_Dal_clean_count$n,
                        sep = "",
                        ". Data from LIRE v.3.0 (Cleaned province = Dalmatia)."),
        title = "Chronological distribution of monuments in Dalmatia",
        subtitle = paste("Using the weighted output of datsteps() ",
                         "with stepsize of ",
-                        attributes(LIRE_all_Dal_clean_scaled)$stepsize,
+                        attributes(LIRE_Dal_clean_scaled)$stepsize,
                         sep = ""))
 
 ggsave("output_images/chronological_distribution/LIRE_clean_dalmatia_plot.jpeg",
        width = 180, height = 100, unit = "mm", dpi = 600)
 
 # now for with a histogram
+##get histogram 
 LIRE_Dal_corpus_clean_histogramscale <- get.histogramscale(LIRE_Dal_corpus_clean_scaled)
-LIRE_all_Dal_clean_scaled_histogram <- get.histogramscale(LIRE_all_Dal_clean_scaled)
+LIRE_Dal_corpus_npf_clean_histogramscale <- get.histogramscale(LIRE_Dal_corpus_npf_clean_scaled)
+LIRE_Dal_clean_scaled_histogram <- get.histogramscale(LIRE_Dal_clean_scaled)
 
+
+## plot with histogram
 ggplot(LIRE_Dal_corpus_clean_scaled, aes(x = DAT_step)) +
   geom_histogram(alpha = 0.8, binwidth = attributes(LIRE_Dal_corpus_clean_scaled)$stepsize,
                  position = "dodge", colour = "black", fill = "#9F522E") +
   stat_density(alpha = 0.8, position = "dodge", colour = "black", fill = "#FF8247",
-               aes(y = (..density.. * LIRE_Dal_corpus_clean_histogramscale), weight = weight)) +
+               aes(y = (after_stat(density) * LIRE_Dal_corpus_clean_histogramscale), weight = weight)) +
   labs(x = "Date (BCE/CE)", y = "Density",
        caption = paste("n = ",
                        LIRE_Dal_corpus_clean_count$n,
                        sep = "",
-                       ". Data from LIRE v.3.0 (Cleaned province = Dalmatia)."),
+                       ". Data from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
+                       "Filtered by key words, tags, and places."),
        title = "Chronological distribution of military monuments in Dalmatia",
      subtitle = paste("Using the weighted output of datsteps() ",
                       "with stepsize of ",
@@ -172,14 +198,34 @@ ggplot(LIRE_Dal_corpus_clean_scaled, aes(x = DAT_step)) +
 ggsave("output_images/chronological_distribution/LIRE_clean_corpus_plot_histogram.jpeg",
        width = 180, height = 100, unit = "mm", dpi = 600)
 
-ggplot(LIRE_all_Dal_clean_scaled, aes(x = DAT_step)) +
-  geom_histogram(alpha = 0.8, binwidth = attributes(LIRE_all_Dal_clean_scaled)$stepsize,
-                 position = "dodge", colour = "black", fill = "#014e3a") +
-  stat_density(alpha = 0.8, position = "dodge", colour = "black", fill = "#009E73",
-               aes(y = (..density.. * LIRE_all_Dal_clean_scaled_histogram), weight = weight)) +
+ggplot(LIRE_Dal_corpus_npf_clean_scaled, aes(x = DAT_step)) +
+  geom_histogram(alpha = 0.8, binwidth = attributes(LIRE_Dal_corpus_npf_clean_scaled)$stepsize,
+                 position = "dodge", colour = "black", fill = "#9F522E") +
+  stat_density(alpha = 0.8, position = "dodge", colour = "black", fill = "#FF8247",
+               aes(y = (after_stat(density) * LIRE_Dal_corpus_npf_clean_histogramscale), weight = weight)) +
   labs(x = "Date (BCE/CE)", y = "Density",
        caption = paste("n = ",
-                       LIRE_all_Dal_clean_count$n,
+                       LIRE_Dal_corpus_npf_clean_count$n,
+                       sep = "",
+                       ". Data from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
+                       "Filtered by key words and tags."),
+       title = "Chronological distribution of military monuments in Dalmatia",
+       subtitle = paste("Using the weighted output of datsteps() ",
+                        "with stepsize of ",
+                        attributes(LIRE_Dal_corpus_npf_clean_scaled)$stepsize,
+                        sep = ""))
+
+ggsave("output_images/chronological_distribution/LIRE_clean_corpus_plot_histogram_no_place_filter.jpeg",
+       width = 180, height = 100, unit = "mm", dpi = 600)
+
+ggplot(LIRE_Dal_clean_scaled, aes(x = DAT_step)) +
+  geom_histogram(alpha = 0.8, binwidth = attributes(LIRE_Dal_clean_scaled)$stepsize,
+                 position = "dodge", colour = "black", fill = "#014e3a") +
+  stat_density(alpha = 0.8, position = "dodge", colour = "black", fill = "#009E73",
+               aes(y = (after_stat(density) * LIRE_Dal_clean_scaled_histogram), weight = weight)) +
+  labs(x = "Date (BCE/CE)", y = "Density",
+       caption = paste("n = ",
+                       LIRE_Dal_clean_count$n,
                        sep = "",
                        ". Data from LIRE v.3.0 (Cleaned province = Dalmatia)."),
        title = "Chronological distribution of monuments in Dalmatia",
@@ -192,7 +238,7 @@ ggsave("output_images/chronological_distribution/LIRE_clean_dalmatia_plot_histog
        width = 180, height = 100, unit = "mm", dpi = 600)
 
 #now to compare
-LIRE_clean_both <- rbind(LIRE_all_Dal_clean_dates_na, LIRE_Dal_corpus_clean_na)
+LIRE_clean_both <- rbind(LIRE_Dal_clean_na, LIRE_Dal_corpus_clean_na)
 
 LIRE_clean_both_scaled <- scaleweight(datsteps(LIRE_clean_both, stepsize = 25),
                                 var = "all")
@@ -207,9 +253,10 @@ ggplot(data = LIRE_clean_both_scaled, aes(x = DAT_step, weight = weight,
        caption = paste("Military n = ",
                        LIRE_Dal_corpus_clean_count$n,
                        ". All n = ",
-                       LIRE_all_Dal_clean_count$n,
+                       LIRE_Dal_clean_count$n,
                        sep = "",
-                       ".\nData from LIRE v.3.0 (Cleaned province = Dalmatia)."),
+                       ".\nData from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
+                       "Filtered by key words, tags, and places."),
        title = "Chronological distribution of monuments in Dalmatia (Density)",
        subtitle = paste("Using the weighted output of datsteps() ",
                         "with stepsize of ",
@@ -217,4 +264,32 @@ ggplot(data = LIRE_clean_both_scaled, aes(x = DAT_step, weight = weight,
                         sep = ""))
        
 ggsave("output_images/chronological_distribution/LIRE_clean_dalmatia_corpus_plot.jpeg",
+       width = 180, height = 100, unit = "mm", dpi = 600)
+
+LIRE_clean_both_npf <- rbind(LIRE_Dal_clean_na, LIRE_Dal_corpus_npf_clean_na)
+
+LIRE_clean_both_scaled_npf <- scaleweight(datsteps(LIRE_clean_both_npf, stepsize = 25),
+                                          var = "all")
+
+ggplot(data = LIRE_clean_both_scaled_npf, aes(x = DAT_step, weight = weight,
+                                          group=variable, fill=variable)) +
+  geom_density(alpha = 0.6) +
+  scale_fill_manual(values = c("Military" = "#FF8247", "All" = "#009E73"),
+                    name = "Category") +
+  labs(x = "Date (BCE/CE)", y = "Density",
+       variable = "Category",
+       caption = paste("Military n = ",
+                       LIRE_Dal_corpus_npf_clean_count$n,
+                       ". All n = ",
+                       LIRE_Dal_clean_count$n,
+                       sep = "",
+                       ".\nData from LIRE v.3.0 (Cleaned province = Dalmatia).\n",
+                       "Filtered by key words and tags."),
+       title = "Chronological distribution of monuments in Dalmatia (Density)",
+       subtitle = paste("Using the weighted output of datsteps() ",
+                        "with stepsize of ",
+                        attributes(LIRE_Dal_corpus_clean_scaled)$stepsize,
+                        sep = ""))
+
+ggsave("output_images/chronological_distribution/LIRE_clean_dalmatia_corpus_plot_no_place_filter.jpeg",
        width = 180, height = 100, unit = "mm", dpi = 600)
