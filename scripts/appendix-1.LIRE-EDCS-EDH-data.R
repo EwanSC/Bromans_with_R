a984838f76d812f28366daa14ad0634ef169ba30
EwanSC <ewansc4@gmail.com>
2024-10-18 15:12:23 +1100

    making viz for EDCS, LIRE, EDH

--- scripts/appendix-1.LIRE-EDCS-EDH-data.R
+++ scripts/appendix-1.LIRE-EDCS-EDH-data.R
@@ -3,10 +3,20 @@
 # 27/09/2024
 # last ran 11/10/2024
 library(dplyr)
+library(stringr)
 library(rjson)
 library(tibble)
 library(data.table)
 library(arrow)
+library(sqldf)
+library(arrow)
+library(ggplot2)
+library(sf)
+library(ggrepel)
+library("gridExtra")
+library("rnaturalearthdata")
+library("rnaturalearth")
+library("rnaturalearthhires")
 
 # LIRE
 LIRE_all <-
@@ -26,7 +36,9 @@ EDCS_Dalmatia <- data.table::rbindlist(EDCS_data, fill = TRUE)
 
 # EDH
 EDH_all <-
- read.csv("https://edh.ub.uni-heidelberg.de/data/download/edh_data_text.csv")
+ read.csv("https://edh.ub.uni-heidelberg.de/data/download/edh_data_text.csv",
+          na = c("","NA","NULL",NULL))
+
 EDH_Dalmatia <- filter(EDH_all, provinz == "Dal")
 
 # count
@@ -65,3 +77,162 @@ database_Dalmatia_salona_count <- Reduce(function(x, y) merge(x, y, all=TRUE),
 
 write.csv(database_Dalmatia_salona_count,
           file = "output_tables/appendices/database_salona_count.csv")
+
+# plot
+## download rnaturalearthhires for basemap
+world <- ne_countries(scale = "large", returnclass = "sf")
+
+## adding road data from DARMC https://hub.arcgis.com/datasets/55a54a1350e14ca0b355d95633da3851_0
+roman_roads <- st_read(
+  "shape_files/Roman_roads.shp")
+
+## adding settlements from Pleiades https://pleiades.stoa.org/downloads
+roman_settlements <- st_read(
+  "data/mapping/Roman_settlements_pleiades.gpkg")
+
+## create df for layer of key sites 
+imp_sites <- data.frame(findspot_ancient_clean=c("Salona",
+                                                 "Narona",
+                                                 "Iader"),
+                        Longitude=c(16.483426,
+                                    17.598611,
+                                    15.223778),
+                        Latitude=c(43.539561,
+                                   43.046389,
+                                   44.115501))
+(imp_sites_ll <- st_as_sf(imp_sites,
+                          coords = c("Longitude",
+                                     "Latitude"),
+                          remove = FALSE,
+                          crs = 4326, agr = "constant"))
+
+## plot LIRE
+LIRE_Dalmatia_place <- na.omit(LIRE_Dalmatia %>%
+                            select(findspot_ancient_clean,
+                                   Longitude,
+                                   Latitude) %>%
+                             group_by(findspot_ancient_clean) %>%
+                             count(findspot_ancient_clean,
+                                   Longitude,
+                                   Latitude) %>%
+                               arrange(desc(n)))
+(LIRE_Dalmatia_ll <- st_as_sf(LIRE_Dalmatia_place, coords = c("Longitude", "Latitude"),
+                                remove = FALSE,
+                                crs = 4326, agr = "constant"))
+LIRE_Dalmatia_n <- count(LIRE_Dalmatia)
+
+ggplot() + 
+geom_sf(data = world, color = "#BEBEBE", fill = "#e4e4e4") + 
+geom_sf(data = roman_roads, colour = "#4D4D4D", size = 0.6) +
+geom_sf(data = roman_settlements, colour = "#4D4D4D", alpha=0.6, size = 0.8) +
+geom_sf(data = LIRE_Dalmatia_ll, aes(size = n), alpha=0.8, colour = "#3468d6") +
+geom_sf(data = imp_sites_ll, colour = "#000000", size = 1) +
+geom_text_repel(data = imp_sites_ll,
+                aes(x = Longitude,
+                    y = Latitude,
+                    label = findspot_ancient_clean),
+                nudge_x = c(-1, -1, -1), 
+                nudge_y = c(-0.25,-0.25,-0.25)) +
+labs(size = "Density",
+     caption = paste("n = ",
+                     LIRE_Dalmatia_n$n,
+                     sep = "",
+                     ".\nEpigraphic data = LIRE v.3.0 (CC BY 4.0).\n",
+                     "Roads = DARMC (CC BY-NC-SA 4.0). Settlements = Pleiades (CC-BY).\n",
+                     "Filtered by key words and tags"),
+                     title = "Distribution of inscriptions",
+                     subtitle = "LIRE") +
+coord_sf(default_crs = st_crs(4326), xlim = c(14, 21), ylim = c(41.5, 46)) +
+theme_void()
+
+ggsave("output_images/appendix-1/01.LIRE_scatter.jpeg",
+       width = 180, height = 140, unit = "mm", dpi = 600)
+
+
+##plot EDCS 
+EDCS_Dalmatia_place <- na.omit(EDCS_Dalmatia %>%
+                            select(place,
+                                   longitude,
+                                   latitude) %>%
+                             group_by(place) %>%
+                             count(place,
+                                   longitude,
+                                   latitude) %>%
+                               arrange(desc(n)))
+
+(EDCS_Dalmatia_ll <- st_as_sf(EDCS_Dalmatia_place, coords = c("longitude", "latitude"),
+                                remove = FALSE,
+                                crs = 4326, agr = "constant"))
+
+EDCS_Dalmatia_n <- count(EDCS_Dalmatia)
+
+ggplot() + 
+geom_sf(data = world, color = "#BEBEBE", fill = "#e4e4e4") + 
+geom_sf(data = roman_roads, colour = "#4D4D4D", size = 0.6) +
+geom_sf(data = roman_settlements, colour = "#4D4D4D", alpha=0.6, size = 0.8) +
+geom_sf(data = EDCS_Dalmatia_ll, aes(size = n), alpha=0.8, colour = "#3468d6") +
+geom_sf(data = imp_sites_ll, colour = "#000000", size = 1) +
+geom_text_repel(data = imp_sites_ll,
+                aes(x = Longitude,
+                    y = Latitude,
+                    label = findspot_ancient_clean),
+                nudge_x = c(-1, -1, -1), 
+                nudge_y = c(-0.25,-0.25,-0.25)) +
+labs(size = "Density",
+     caption = paste("n = ",
+                     EDCS_Dalmatia_n$n,
+                     sep = "",
+                     ".\nEpigraphic data = EDCS.\n",
+                     "Roads = DARMC (CC BY-NC-SA 4.0). Settlements = Pleiades (CC-BY).\n",
+                     "Filtered by key words and tags"),
+                     title = "Distribution of inscriptions",
+                     subtitle = "EDCS") +
+coord_sf(default_crs = st_crs(4326), xlim = c(14, 21), ylim = c(41.5, 46)) +
+theme_void()
+
+ggsave("output_images/appendix-1/02.EDCS_scatter.jpeg",
+       width = 180, height = 140, unit = "mm", dpi = 600)
+
+##plot EDH
+EDH_Dalmatia_place <- na.omit(EDH_Dalmatia %>%
+                            select(fo_antik,
+                                   koordinaten1) %>%
+                             group_by(fo_antik) %>%
+                             count(fo_antik,
+                                   koordinaten1) %>%
+                               arrange(desc(n)))
+
+EDH_Dalmatia_place[c('latitude', 'longitude')] <- str_split_fixed(EDH_Dalmatia_place$koordinaten1, ',', 2)
+
+(EDH_Dalmatia_ll <- st_as_sf(EDH_Dalmatia_place, coords = c("longitude", "latitude"),
+                             remove = FALSE,
+                             crs = 4326, agr = "constant"))
+
+EDH_Dalmatia_n <- count(EDH_Dalmatia)
+
+ggplot() + 
+geom_sf(data = world, color = "#BEBEBE", fill = "#e4e4e4") + 
+geom_sf(data = roman_roads, colour = "#4D4D4D", size = 0.6) +
+geom_sf(data = roman_settlements, colour = "#4D4D4D", alpha=0.6, size = 0.8) +
+geom_sf(data = EDH_Dalmatia_ll, aes(size = n), alpha=0.8, colour = "#3468d6") +
+geom_sf(data = imp_sites_ll, colour = "#000000", size = 1) +
+geom_text_repel(data = imp_sites_ll,
+                aes(x = Longitude,
+                    y = Latitude,
+                    label = findspot_ancient_clean),
+                nudge_x = c(-1, -1, -1), 
+                nudge_y = c(-0.25,-0.25,-0.25)) +
+labs(size = "Density",
+     caption = paste("n = ",
+                     EDH_Dalmatia_n$n,
+                     sep = "",
+                     ".\nEpigraphic data = EDH (CC BY-SA 4.0).\n",
+                     "Roads = DARMC (CC BY-NC-SA 4.0). Settlements = Pleiades (CC-BY).\n",
+                     "Filtered by key words and tags"),
+                     title = "Distribution of inscriptions",
+                     subtitle = "EDH") +
+coord_sf(default_crs = st_crs(4326), xlim = c(14, 21), ylim = c(41.5, 46)) +
+theme_void()
+
+ggsave("output_images/appendix-1/02.EDH_scatter.jpeg",
+       width = 180, height = 140, unit = "mm", dpi = 600)
